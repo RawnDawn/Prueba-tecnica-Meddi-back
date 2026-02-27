e8d670e879053ea0e151113c7bc6d695a8bd63c3
RawnDawn <angel.dev@hunabku.com.mx>
2026-02-27 11:19:16 -0600

    feat: scaffold and important packages

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"
+import app from "@/app"
+import { connectDB } from "@/config/db"
+
+dotenv.config()
+
+connectDB();
+
+const PORT = process.env.APP_PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
